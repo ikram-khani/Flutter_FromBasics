6b5b97247953c628aee02929c4e7dd9689fa5cc8
Ikram khan <ikr302000@gmail.com>
2022-07-12 14:07:45 +0500

    custom widget

--- custom_widget/lib/customTxtField.dart
+++ custom_widget/lib/customTxtField.dart
@@ -2,20 +2,17 @@ import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
   final String label;
-  final String hint; //not valid condition
-  String errorMessage;
+  final String hint;
   final bool boolobscureText;
-  bool notvalid;
   bool pwd;
   bool isnumber;
-  CustomTextField(
-      {this.label = "",
-      this.hint = "",
-      this.errorMessage = "",
-      this.boolobscureText = false,
-      this.pwd = false,
-      this.isnumber = false,
-      this.notvalid = false});
+  CustomTextField({
+    this.label = "",
+    this.hint = "",
+    this.boolobscureText = false,
+    this.pwd = false,
+    this.isnumber = false,
+  });
   //true or false
 
   @override
@@ -23,32 +20,18 @@ class CustomTextField extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: TextFormField(
-          obscureText: boolobscureText,
-          decoration: InputDecoration(
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-              labelStyle: TextStyle(fontSize: 15),
-              hintStyle: TextStyle(fontSize: 15),
-              label: Text(label),
-              hintText: hint,
-              errorText: notvalid ? errorMessage : null,
-              errorStyle: TextStyle(
-                fontSize: 15,
-              )),
-          keyboardType: isnumber ? TextInputType.phone : TextInputType.text,
-          validator: (value) {
-            if (pwd = false) {
-              if (value!.isEmpty) {
-                notvalid = true;
-              }
-            }
-            if (pwd = true) {
-              if (value!.length < 8) {
-                notvalid = true;
-                errorMessage = "Password must be atleast 8 characters";
-              }
-            }
-          }),
+        obscureText: boolobscureText,
+        decoration: InputDecoration(
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+            labelStyle: TextStyle(fontSize: 15),
+            hintStyle: TextStyle(fontSize: 15),
+            label: Text(label),
+            hintText: hint,
+            errorStyle: TextStyle(
+              fontSize: 15,
+            )),
+        keyboardType: isnumber ? TextInputType.phone : TextInputType.text,
+      ),
     );
   }
 }
